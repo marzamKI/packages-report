b7dbe4337b1aecd212b83f98df87224f0ea4a857
Margherita Zamboni <margherita.zamboni@ki.se>
2019-06-11 10:01:37 +0200

    Begin to study installed packages

---
+++ explore-packages.R
@@ -0,0 +1,11 @@
+.libPaths() # Get paths for where packages are installed
+
+#install.packages("tidyverse")
+#install.packages("here")
+
+library(tidyverse)
+
+ipt <- installed.packages() %>% 
+  as_tibble() %>% # Tidyverse dataframe that give a nice print method
+  select(Package, LibPath, Priority, Built)
+ipt 
